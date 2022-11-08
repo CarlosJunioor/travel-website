22419d9e90b17acb384ae61d579de1c951df04fc
CarlosJunioor <carlosjunior866@gmail.com>
2022-11-08 19:55:10 +0000

    fix: package and herosection

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -9,22 +9,22 @@ const HeroSection = () => {
   return (
     <div className='hero-container'>
         <video src={video2} autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
-        <p>What are you waiting for?</p>
+        <h1>Modelos Atómicos</h1>
+        <p>Como é constituída a Estrutura da Matéria?</p>
         <div className='hero-btns'>
             <Button 
                 className='btns' 
                 buttonStyle='btn--outline' 
                 buttonSize='btn--large'
             >
-                GET STARTED
+                Learn
             </Button>
             <Button 
                 className='btns' 
                 buttonStyle='btn--primary' 
                 buttonSize='btn--large'
             >
-                WATCH TRAILER <i className='far fa-play-circle' /> 
+                ASSISTA AO TRAILER <i className='far fa-play-circle' /> 
             </Button>
         </div>
     </div>
